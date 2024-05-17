d173088f57916d82e75e949f5d6fce38e74da1cb
Alpez Developer <s.rodriguezs234@uniandes.edu.co>
2024-05-17 09:43:26 -0500

    Parcial 2

--- src/app/naves/naves.module.ts
+++ src/app/naves/naves.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavesComponent } from './naves.component';
+import { NaveDetailComponent } from './naveDetail/naveDetail.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavesComponent],
-  exports: [NavesComponent]
+  declarations: [NavesComponent, NaveDetailComponent],
+  exports: [NavesComponent, NaveDetailComponent]
 })
 export class NavesModule { }
